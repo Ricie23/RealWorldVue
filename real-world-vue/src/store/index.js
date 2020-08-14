78cf0f5f1acca272692c99fe3db89d3fbff79505
Rich <rrobertson@tentcraft.com>
2020-08-14 18:35:13 -0400

    states and getters

--- real-world-vue/src/store/index.js
+++ real-world-vue/src/store/index.js
@@ -4,8 +4,41 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: {id: 'abc', name: 'Bob'},
+    categories: [
+      'sustainability', 'nature', 
+      'animal welfare', 'housing',
+      'education', 'food', 'community'
+    ],
+    todos: [
+      {id: 1, text: 'this', done:true},
+      {id: 2, text: 'is', done:true},
+      {id: 3, text: 'a', done:true},
+      {id: 4, text: 'todo list', done:true},
+    ],
+    events: [
+      {id: 1, title: 'these', organizer: 'Bob'},
+      {id: 2, title: 'are', organizer: 'Bob'},
+      {id: 3, title: 'some', organizer: 'Bob'},
+      {id: 4, title: 'events', organizer: 'Bob'},
+    ]
+  },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
+  getters: {
+    catLength: state =>{
+      return state.categories.length
+    },
+    doneTodos: state=>{
+      return state.todos.filter(todo => todo.done)
+    },
+    activeTodosCount: (state) => {
+      return state.todos.filter(todo => !todo.done).length
+    },
+    getEventById: state => id =>{
+      return state.events.find(event => event.id == id)
+    }
+  }
 });
--- real-world-vue/src/views/EventCreate.vue
+++ real-world-vue/src/views/EventCreate.vue
@@ -1,3 +1,30 @@
 <template>
-  <h1>create an event</h1>
+  <div>
+<h1>Create an Event, {{user.name}}</h1>
+<p>this event is created by {{user.id}}</p>
+<p>there are {{catLength}} categories</p>
+<p>{{ getEventById(3) }}</p>
+<ul>
+  <li v-for="cat in categories" :key="cat">{{cat}}</li>
+</ul>
+  </div>
 </template>
+
+<script>
+import{mapState, mapGetters} from 'vuex'
+  export default {
+    computed: {
+
+      ...mapGetters(['getEventById', 'catLength']),
+      ...mapState(['user','categories'])
+        
+    },
+        
+
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
